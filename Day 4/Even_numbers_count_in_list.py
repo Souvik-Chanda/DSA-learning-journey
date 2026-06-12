fabd25777a32d0adcba5ebded185d6e56c3c812e
Souvik-Chanda <souvikchanda07@gmail.com>
2026-06-12 23:37:25 +0530

    Add files via upload

---
+++ Day 4/Even_numbers_count_in_list.py
@@ -0,0 +1,6 @@
+numbers = [4, 7, 10, 3, 8]
+count =0
+for num in numbers :
+    if num % 2 == 0 :
+        count = count +1
+print(count)        
---
+++ Day 4/even_and_odd_numbers_count_in_list.py
@@ -0,0 +1,10 @@
+numbers = [2, 5, 8, 7, 10]
+even_count = 0
+odd_count = 0
+for num in numbers :
+    if num % 2 == 0 :
+        even_count = even_count +1
+    else :
+        odd_count = odd_count +1
+print("Even numbers count = ",even_count)            
+print("Odd numbers count = ",odd_count)
